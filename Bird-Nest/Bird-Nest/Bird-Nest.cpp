6183573eaee074da39149a95b8014e844f2ffa4c
Fekete Márk <fmark98@gmail.com>
2019-10-01 19:41:06 +0200

    Maybe finished and bugfixed #1;

--- Bird-Nest/Bird-Nest/Bird-Nest.cpp
+++ Bird-Nest/Bird-Nest/Bird-Nest.cpp
@@ -16,14 +16,15 @@ int main()
 	int birdCount = 0;
 	int areaSize = 0;
 	Teritory territorys[MAX_BIRD_NESTS];
-	int area[MAX_AREA_SIZE][MAX_AREA_SIZE] = { -1 };
-	//for (int i = 0; i < MAX_AREA_SIZE; ++i) // Set all field to -1 representing thet isn't part of the active area.
-	//{
-	//	for (int j = 0; j < MAX_AREA_SIZE; ++j)
-	//	{
-	//		area[i][j] = -1;
-	//	}
-	//}
+	int area[MAX_AREA_SIZE][MAX_AREA_SIZE];
+
+	for (int i = 0; i < MAX_AREA_SIZE; ++i) // Set all field to -1 representing thet isn't part of the active area.
+	{
+		for (int j = 0; j < MAX_AREA_SIZE; ++j)
+		{
+			area[i][j] = -1;
+		}
+	}
 
 	cin >> birdCount >> areaSize;
 
@@ -42,19 +43,36 @@ int main()
 
 	for (int i = 0; i < birdCount; ++i)
 	{
-		int currentX = territorys[i].posX;
-		int currentY = territorys[i].posY;
-		int endX = currentX + territorys[i].size;
-		int endY = currentY + territorys[i].size;
-		for (int j = currentX; j <= endX; ++j)
+		int currentNestX = territorys[i].posX - 1;
+		int currentNestY = territorys[i].posY - 1;
+		int startTerritoryX = currentNestX - territorys[i].size;
+		int startTerritoryY = currentNestY - territorys[i].size;
+		int endTerritoryX = currentNestX + territorys[i].size;
+		int endTerritoryY = currentNestY + territorys[i].size;
+		for (int j = startTerritoryX; j <= endTerritoryX; ++j)
 		{
-			for (int k = currentY; k < endY; k++)
+			for (int k = startTerritoryY; k <= endTerritoryY; k++)
 			{
-				area[j][k] = 1;
+				if (j >= 0 && k >= 0 && j < areaSize && k < areaSize)
+				{
+					area[j][k] = 1;
+				}
+			}
+		}
+		area[currentNestX][currentNestY] = 2;
+	}
+
+	int emptyFieldCount = 0;
+	for (int i = 0; i < areaSize; ++i)
+	{
+		for (int j = 0; j < areaSize; ++j)
+		{
+			if (area[i][j] == 0)
+			{
+				++emptyFieldCount;
 			}
 		}
 	}
-
-	cout << "Hello World!\n";
+	cout << emptyFieldCount;
 	return 0;
 }
